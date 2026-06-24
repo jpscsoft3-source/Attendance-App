fc10444daa8b9fa5e30d051110521dfff99fec03
JPSCSOFT ENGINEER <jpsc.soft3@gmail.com>
2026-06-24 12:57:49 +0530

    Firbaseconfig

--- src/firebase.js
+++ src/firebase.js
@@ -9,24 +9,24 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDJtX7CBcXd0acX-1oIO5n1no41WRAASUg",
-//   authDomain: "attendance-leave-management.firebaseapp.com",
-//   projectId: "attendance-leave-management",
-//   storageBucket: "attendance-leave-management.firebasestorage.app",
-//   messagingSenderId: "471762551739",
-//   appId: "1:471762551739:web:ad29bb4d1ca15e477c7a9e",
-//   measurementId: "G-9NC1XD2YGP"
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA2V84qMJ-THgJqX0fEC9KS-DX_3At02JQ",
-  authDomain: "attendence-locahost.firebaseapp.com",
-  projectId: "attendence-locahost",
-  storageBucket: "attendence-locahost.firebasestorage.app",
-  messagingSenderId: "992561848299",
-  appId: "1:992561848299:web:d67ea1464f65b0a82a43ae"
+  apiKey: "AIzaSyDJtX7CBcXd0acX-1oIO5n1no41WRAASUg",
+  authDomain: "attendance-leave-management.firebaseapp.com",
+  projectId: "attendance-leave-management",
+  storageBucket: "attendance-leave-management.firebasestorage.app",
+  messagingSenderId: "471762551739",
+  appId: "1:471762551739:web:ad29bb4d1ca15e477c7a9e",
+  measurementId: "G-9NC1XD2YGP"
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA2V84qMJ-THgJqX0fEC9KS-DX_3At02JQ",
+//   authDomain: "attendence-locahost.firebaseapp.com",
+//   projectId: "attendence-locahost",
+//   storageBucket: "attendence-locahost.firebasestorage.app",
+//   messagingSenderId: "992561848299",
+//   appId: "1:992561848299:web:d67ea1464f65b0a82a43ae"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
